11c9587c7e60c28171c5e9134423baad474e405c
Matthew Mitchell <mattlmitchell@proton.me>
2025-04-02 22:30:21 +0100

    Add TaprootTransactionSignatureMetadata

--- lib/src/api/types/signature_metadata.dart
+++ lib/src/api/types/signature_metadata.dart
@@ -1,25 +1,41 @@
 import 'dart:typed_data';
 import 'package:coinlib/coinlib.dart' as cl;
+import 'package:collection/collection.dart';
+import 'package:noosphere_roast_client/src/common/serial.dart';
+
+/// Thrown when the metadata is invalid. Other specific exceptions may be thrown
+/// for invalid metadata.
+class InvalidMetaData implements Exception {
+  final String message;
+  InvalidMetaData(this.message);
+  @override
+  String toString() => "InvalidMetaData: $message";
+}
 
 const int _emptyType = 0;
+const int _taprootTxType = 1;
 
 abstract interface class SignatureMetadata with cl.Writable {
 
   int get type;
 
+  /// May throw an exception other than [cl.OutOfData] if the data is invalid
   static SignatureMetadata fromReader(cl.BytesReader reader)
     => switch(reader.readUInt8()) {
       _emptyType => EmptySignatureMetadata(),
+      _taprootTxType => TaprootTransactionSignatureMetadata._fromReader(reader),
       int i => UnknownSignatureMetadata(
         i, reader.bytes.buffer.asUint8List(reader.offset),
       ),
     };
 
   /// Convenience constructor to construct from serialised [bytes].
+  /// May throw an exception other than [cl.OutOfData] if the data is invalid
   static SignatureMetadata fromBytes(Uint8List bytes)
     => SignatureMetadata.fromReader(cl.BytesReader(bytes));
 
   /// Convenience constructor to construct from encoded [hex].
+  /// May throw an exception other than [cl.OutOfData] if the data is invalid
   static SignatureMetadata fromHex(String hex)
     => SignatureMetadata.fromBytes(cl.hexToBytes(hex));
 
@@ -37,6 +53,162 @@ class EmptySignatureMetadata extends SignatureMetadata {
   @override
   void write(cl.Writer writer) {
     _writeType(writer);
+  }
+
+}
+
+class TaprootTransactionSignatureMetadata extends SignatureMetadata {
+
+  @override
+  final int type = _taprootTxType;
+
+  /// The transaction being signed
+  final cl.Transaction transaction;
+  /// The details for each input to be signed
+  final List<cl.TaprootSignDetails> signDetails;
+
+  /// Takes the [transaction] to be signed and the [signDetails] for all inputs
+  /// to sign which must carry the same [transaction] object.
+  TaprootTransactionSignatureMetadata({
+    required this.transaction,
+    required this.signDetails,
+  }) {
+
+    if (signDetails.any((details) => details.tx != transaction)) {
+      throw InvalidMetaData("all details should carry the transaction object");
+    }
+
+    if (
+      signDetails.map((details) => details.inputN).toSet().length
+      != signDetails.length
+    ) {
+      throw InvalidMetaData("should have unique input indicies");
+    }
+
+    for (final details in signDetails) {
+      final isKeyPath
+        = transaction.inputs[details.inputN] is cl.TaprootKeyInput;
+      final isKeyDetails = details is cl.TaprootKeySignDetails;
+      if (isKeyPath != isKeyDetails) {
+        throw InvalidMetaData("input type doesn't correspond to sign details");
+      }
+    }
+
+  }
+
+  // Read, not including the type byte
+  factory TaprootTransactionSignatureMetadata._fromReader(
+    cl.BytesReader reader,
+  ) {
+
+    final transaction = cl.Transaction.fromReader(reader);
+    final inLen = transaction.inputs.length;
+
+    final prevOuts = List<cl.Output?>.generate(
+      inLen,
+      (i) => reader.readBool() ? cl.Output.fromReader(reader) : null,
+    );
+
+    final List<cl.TaprootSignDetails> signDetails = [];
+
+    final detailsLen = reader.readUInt16();
+    for (int i = 0; i < detailsLen; i++) {
+
+      final inputN = reader.readUInt16();
+      if (inputN >= inLen) {
+        throw InvalidMetaData("Input N doesn't exist in transaction");
+      }
+
+      final hashValue = reader.readUInt8();
+      if (!cl.SigHashType.validValue(hashValue)) {
+        throw InvalidMetaData("Invalid sighash value");
+      }
+      final hashType = cl.SigHashType.fromValue(hashValue);
+      final isScript = reader.readBool();
+      final leafHash = reader.readBool() ? reader.readSlice(32) : null;
+      final codeSeperatorPos = reader.readUInt32();
+
+      final inPrevOuts = hashType.allInputs
+        ? prevOuts
+        : (hashType.anyPrevOutAnyScript ? <cl.Output>[] : [prevOuts[inputN]]);
+
+      signDetails.add(
+        cl.TaprootSignDetails(
+          tx: transaction,
+          inputN: inputN,
+          prevOuts: inPrevOuts.nonNulls.toList(),
+          isScript: isScript,
+          hashType: hashType,
+          leafHash: leafHash,
+          codeSeperatorPos: codeSeperatorPos,
+        ),
+      );
+
+    }
+
+    return TaprootTransactionSignatureMetadata(
+      transaction: transaction,
+      signDetails: signDetails,
+    );
+
+  }
+
+  // Collect previous outputs that are required for the inputs to be signed. For
+  // each transaction input, either an output is provided or null is given if
+  // that output is not required.
+  List<cl.Output?> _collectPrevOuts() {
+
+    // First look for input sign details that signs for all inputs and carries
+    // all previous outputs.
+    final allPrevouts = signDetails.firstWhereOrNull(
+      (details) => details.hashType.allInputs,
+    )?.prevOuts;
+
+    if (allPrevouts != null) return allPrevouts;
+
+    // Collect previous outputs for each sign details
+    final prevOuts = List<cl.Output?>.filled(transaction.inputs.length, null);
+    for (final details in signDetails) {
+      if (details.prevOuts.isEmpty) continue;
+      prevOuts[details.inputN] = details.prevOuts.first;
+    }
+
+    return prevOuts;
+
+  }
+
+  @override
+  void write(cl.Writer writer) {
+
+    _writeType(writer);
+    transaction.write(writer);
+
+    // Write all previous outputs that can be determined from the sign details
+    final prevOuts = _collectPrevOuts();
+    for (final out in prevOuts) {
+      writer.writeBool(out != null);
+      if (out != null) out.write(writer);
+    }
+
+    // Write per input sign details
+    writer.writeUInt16(signDetails.length);
+    for (final details in signDetails) {
+
+      // Prevouts are not included here as they are collected and included
+      // separately for the entire transaction.
+
+      writer.writeUInt16(details.inputN);
+      writer.writeUInt8(details.hashType.value);
+      writer.writeBool(details.isScript);
+
+      final leafHash = details.leafHash;
+      writer.writeBool(leafHash != null);
+      if (leafHash != null) writer.writeSlice(leafHash);
+
+      writer.writeUInt32(details.codeSeperatorPos);
+
+    }
+
   }
 
 }
--- test/api/types/signature_metadata_test.dart
+++ test/api/types/signature_metadata_test.dart
@@ -1,8 +1,11 @@
+import 'dart:typed_data';
 import 'package:coinlib/coinlib.dart' as cl;
 import 'package:test/test.dart';
 import 'package:noosphere_roast_client/noosphere_roast_client.dart';
+import '../../data.dart';
 
 void main() {
+
   group("SignatureMetadata", () {
 
     setUpAll(loadFrosty);
@@ -23,4 +26,211 @@ void main() {
     });
 
   });
+
+  group("TaprootTransactionSignatureMetadata", () {
+
+    late final cl.Transaction tx;
+    late final List<cl.Output> trOuts;
+    late final cl.TapLeafChecksig apoLeaf;
+    late final cl.TaprootSignDetails apoDetails, apoasDetails;
+    setUpAll(() async {
+
+      await loadFrosty();
+
+      final key = getPrivkey(0);
+      apoLeaf = cl.TapLeafChecksig.apo(key.pubkey);
+      final tr = cl.Taproot(internalKey: key.pubkey, mast: apoLeaf);
+
+      trOuts = List.generate(
+        4,
+        (i) => cl.Output.fromProgram(
+          BigInt.from((i+1)*1000000),
+          cl.P2TR.fromTaproot(tr),
+        ),
+      );
+
+      final dummyHash = Uint8List(32)..last = 1;
+
+      tx = cl.Transaction(
+        inputs: [
+          cl.TaprootKeyInput(prevOut: cl.OutPoint(dummyHash, 0)),
+          cl.TaprootSingleScriptSigInput.anyPrevOut(
+            taproot: tr,
+            leaf: apoLeaf,
+          ),
+          cl.TaprootSingleScriptSigInput.anyPrevOutAnyScript(),
+          cl.P2PKHInput(
+            prevOut: cl.OutPoint(dummyHash, 4),
+            publicKey: key.pubkey,
+          ),
+        ],
+        outputs: [trOuts.first],
+      );
+
+      apoasDetails = cl.TaprootScriptSignDetails(
+        tx: tx,
+        inputN: 2,
+        prevOuts: [],
+        hashType: cl.SigHashType.all(
+          inputs: cl.InputSigHashOption.anyPrevOutAnyScript,
+        ),
+      );
+      apoDetails = cl.TaprootScriptSignDetails(
+        tx: tx,
+        inputN: 1,
+        prevOuts: [trOuts[1]],
+        leafHash: apoLeaf.hash,
+        hashType: cl.SigHashType.all(
+          inputs: cl.InputSigHashOption.anyPrevOut,
+        ),
+      );
+
+    });
+
+    void expectSignDetails(
+      cl.TaprootSignDetails actual, cl.TaprootSignDetails expected,
+    ) {
+      expect(actual.isScript, expected.isScript);
+      expect(actual.leafHash, expected.leafHash);
+      expect(actual.codeSeperatorPos, expected.codeSeperatorPos);
+      expect(actual.inputN, expected.inputN);
+      expect(actual.hashType, expected.hashType);
+      expect(
+        actual.prevOuts.map((out) => out.toHex()),
+        expected.prevOuts.map((out) => out.toHex()),
+      );
+    }
+
+    test("retains details after write/read", () {
+
+      for (final signDetails in [
+        // Contains SIGHASH_DEFAULT with all prevouts
+        [
+          cl.TaprootKeySignDetails(tx: tx, inputN: 0, prevOuts: trOuts),
+          apoasDetails,
+          apoDetails,
+        ],
+        // Contains only AOCP, APO and APOAS
+        [
+          apoasDetails,
+          apoDetails,
+          cl.TaprootKeySignDetails(
+            tx: tx,
+            inputN: 0,
+            prevOuts: [trOuts.first],
+            hashType: cl.SigHashType.all(
+              inputs: cl.InputSigHashOption.anyOneCanPay,
+            ),
+          ),
+        ],
+      ]) {
+
+        final metadata = TaprootTransactionSignatureMetadata(
+          transaction: tx,
+          signDetails: signDetails,
+        );
+
+        final actual = SignatureMetadata.fromBytes(metadata.toBytes())
+          as TaprootTransactionSignatureMetadata;
+        expect(actual.transaction.toHex(), tx.toHex());
+
+        // All sign details share the same tx object
+        expect(
+          {
+            actual.transaction,
+            ...actual.signDetails.map((details) => details.tx),
+          },
+          hasLength(1),
+        );
+
+        for (int i = 0; i < metadata.signDetails.length; i++) {
+          expectSignDetails(actual.signDetails[i], metadata.signDetails[i]);
+        }
+
+      }
+
+    });
+
+    test("invalid", () {
+
+      void expectInvalid(List<cl.TaprootSignDetails> details) => expect(
+        () => TaprootTransactionSignatureMetadata(
+          transaction: tx,
+          signDetails: details,
+        ),
+        throwsException,
+      );
+
+      // Wrong tx object
+      expectInvalid(
+        [
+          apoDetails,
+          cl.TaprootKeySignDetails(
+            tx: tx.addOutput(trOuts.first),
+            inputN: 0,
+            prevOuts: trOuts,
+          ),
+        ],
+      );
+
+      // Duplicate input N
+      expectInvalid(
+        [
+          cl.TaprootKeySignDetails(tx: tx, inputN: 0, prevOuts: trOuts),
+          cl.TaprootKeySignDetails(
+            tx: tx,
+            inputN: 0,
+            prevOuts: trOuts,
+            hashType: cl.SigHashType.single(),
+          ),
+        ],
+      );
+
+      // Input not key-path TR
+      expectInvalid(
+        [cl.TaprootKeySignDetails(tx: tx, inputN: 1, prevOuts: trOuts)],
+      );
+
+      // Input not script-path TR
+      expectInvalid(
+        [cl.TaprootScriptSignDetails(tx: tx, inputN: 0, prevOuts: trOuts)],
+      );
+
+      // Input not TR
+      expectInvalid(
+        [cl.TaprootKeySignDetails(tx: tx, inputN: 3, prevOuts: trOuts)],
+      );
+
+    });
+
+    test("invalid bytes", () {
+
+      void expectInvalid(String hex) => expect(
+        () => SignatureMetadata.fromHex(hex),
+        throwsA(anyOf(isA<InvalidMetaData>(), isA<cl.CannotSignInput>())),
+      );
+
+      final typeAndTxHex = "01${tx.toHex()}";
+      final out1Hex = "0140420f000000000022512001bff6ce4385e42d5b5f9c3c671c7b1f854952306e912b4deca4b60b29645a37";
+      final out2Hex = "0180841e000000000022512001bff6ce4385e42d5b5f9c3c671c7b1f854952306e912b4deca4b60b29645a37";
+      final out3Hex = "01c0c62d000000000022512001bff6ce4385e42d5b5f9c3c671c7b1f854952306e912b4deca4b60b29645a37";
+      final out4Hex = "0100093d000000000022512001bff6ce4385e42d5b5f9c3c671c7b1f854952306e912b4deca4b60b29645a37";
+      final txAndAllOuts = "$typeAndTxHex$out1Hex$out2Hex$out3Hex$out4Hex";
+
+      // Invalid inputN
+      expectInvalid("${txAndAllOuts}01000400000000ffffffff");
+
+      // Invalid sighash
+      expectInvalid("${txAndAllOuts}01000000040000ffffffff");
+
+      // Missing output for SIGHASH_DEFAULT
+      expectInvalid("$typeAndTxHex$out1Hex$out2Hex${out3Hex}0001000000000000ffffffff");
+
+      // Missing specific output for APO
+      expectInvalid("${typeAndTxHex}0000${out3Hex}0001000100410000ffffffff");
+
+    });
+
+  });
+
 }
